a6c9ff1b6d314641f674bc6c3899936816b8b4c9
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-29 21:49:27 -0500

    initial commit

---
+++ hangman.py
@@ -0,0 +1,11 @@
+#Step 1
+
+word_list = ['ketchup', 'chocolate', 'camel']
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make
+# guess lowercase.
+
+#TODO-3 - check if the letter the user guessed (guess) is on of the letters in the chosen_word.
+
