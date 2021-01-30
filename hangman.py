f9a6b89a4d08805ce764e3ebc1770b7ed64b4f74
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-30 10:16:17 -0500

    added counter to count incorrect guesses

--- hangman.py
+++ hangman.py
@@ -1,26 +1,25 @@
 from random import choice
 
-#Step 1
-
 word_list = ['ketchup', 'chocolate', 'camel']
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+counter = 7
 chosen_word = choice(word_list)
 print(chosen_word)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make
-# guess lowercase.
 display = ['_' for letter in chosen_word]
 print(display)
-# guess = input('Guess a letter: ').lower()
-# while(len(guess) is not 1):
-#     guess = input('Should be only 1 letter, guess again: ').lower()
 
-#TODO-3 - check if the letter the user guessed (guess) is on of the letters in the chosen_word.
 while ''.join(display) != chosen_word:
     guess = input('Guess a letter: ').lower()
     while(len(guess) is not 1):
         guess = input('Should be only 1 letter, guess again: ').lower()
+
+    if guess not in chosen_word:
+        print(f'{guess} was not found')
+        counter -= 1
+        print(counter)
+        if counter == 0:
+            print('game over')
+            break
 
     for index in range(0, len(chosen_word)):
         if guess == chosen_word[index]:
