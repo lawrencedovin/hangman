a5ee2b6a22602ff95f1f7a322aa368e1ea174d68
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-29 21:53:11 -0500

    Added randomly chosen word

--- hangman.py
+++ hangman.py
@@ -1,8 +1,12 @@
+from random import choice
+
 #Step 1
 
 word_list = ['ketchup', 'chocolate', 'camel']
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = choice(word_list)
+print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make
 # guess lowercase.
