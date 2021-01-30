ba877bc7e272760f023a2b5413c8db31b0a1fbc0
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-30 10:07:03 -0500

    added loop to check guess

--- hangman.py
+++ hangman.py
@@ -10,12 +10,23 @@ print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make
 # guess lowercase.
-guess = input('Guess a letter: ').lower()
-while(len(guess) is not 1):
-    guess = input('Should be only 1 letter, guess again: ').lower()
+display = ['_' for letter in chosen_word]
+print(display)
+# guess = input('Guess a letter: ').lower()
+# while(len(guess) is not 1):
+#     guess = input('Should be only 1 letter, guess again: ').lower()
 
 #TODO-3 - check if the letter the user guessed (guess) is on of the letters in the chosen_word.
-if guess in chosen_word:
-    print(f'{guess} was found')
-else:
-    print(f'{guess} was not found')
+while ''.join(display) != chosen_word:
+    guess = input('Guess a letter: ').lower()
+    while(len(guess) is not 1):
+        guess = input('Should be only 1 letter, guess again: ').lower()
+
+    for index in range(0, len(chosen_word)):
+        if guess == chosen_word[index]:
+            display[index] = guess
+        else:
+            display[index] = display[index]
+    print(display)
+    print(''.join(display))
+    
