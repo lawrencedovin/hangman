cb76a5450d740d8649344a249f853ac6c526ff06
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-31 19:34:43 -0500

    Added hangman ascii art and finished game

--- hangman.py
+++ hangman.py
@@ -1,15 +1,18 @@
 from random import choice
+from art import stages, logo
 
 word_list = ['ketchup', 'chocolate', 'camel']
-counter = 7
+counter = 6
 chosen_word = choice(word_list)
-print(chosen_word)
-
+print(logo)
 display = ['_' for letter in chosen_word]
-print(display)
 
 while ''.join(display) != chosen_word:
-    guess = input('Guess a letter: ').lower()
+    output = ''.join(str(char) + ' ' for char in display)
+    print(stages[counter])
+    print(output)
+
+    guess = input('\nGuess a letter: ').lower()
     while(len(guess) is not 1):
         guess = input('Should be only 1 letter, guess again: ').lower()
     
@@ -21,18 +24,20 @@ while ''.join(display) != chosen_word:
         counter -= 1
         print(counter)
         if counter == 0:
-            print('game over')
+            print(stages[counter])
+            print('Game over, you lose')
             break
+    else: 
+        print(f'{guess} was found')
 
     for index in range(0, len(chosen_word)):
         if guess == chosen_word[index]:
             display[index] = guess
         else:
             display[index] = display[index]
-    print(display)
     output = ''.join(str(char) + ' ' for char in display)
-    print(output)
+    
 
 if ''.join(display) == chosen_word:
-    print(f'Congrats you guessed {chosen_word} corrently and won the game!')
+    print(f'Congrats you guessed {chosen_word} correctly and won the game!')
     
