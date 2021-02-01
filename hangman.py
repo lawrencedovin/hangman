aca1186b8c8cbea0adb0e50054930f6e1f53bcdd
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-31 20:21:36 -0500

    refactored and added word list

--- hangman.py
+++ hangman.py
@@ -1,15 +1,15 @@
 from random import choice
 from art import stages, logo
+from words import word_list
 
-word_list = ['ketchup', 'chocolate', 'camel']
-counter = 6
+lives = len(stages) - 1
 chosen_word = choice(word_list)
 print(logo)
 display = ['_' for letter in chosen_word]
 
 while ''.join(display) != chosen_word:
     output = ''.join(str(char) + ' ' for char in display)
-    print(stages[counter])
+    print(stages[lives])
     print(output)
 
     guess = input('\nGuess a letter: ').lower()
@@ -21,23 +21,19 @@ while ''.join(display) != chosen_word:
 
     if guess not in chosen_word:
         print(f'{guess} was not found')
-        counter -= 1
-        print(counter)
-        if counter == 0:
-            print(stages[counter])
-            print('Game over, you lose')
+        lives -= 1
+        if lives == 0:
+            print(stages[lives])
             break
     else: 
         print(f'{guess} was found')
 
     for index in range(0, len(chosen_word)):
-        if guess == chosen_word[index]:
-            display[index] = guess
-        else:
-            display[index] = display[index]
-    output = ''.join(str(char) + ' ' for char in display)
+        display[index] = guess if guess == chosen_word[index] else display[index]
     
-
 if ''.join(display) == chosen_word:
     print(f'Congrats you guessed {chosen_word} correctly and won the game!')
+else:
+    print(f'Game over, you lose. the word was {chosen_word}')
+
     
